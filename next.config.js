64bafab267262a6a2481643671f4df079ac5c840
owendominicantours-prog <owendominicantours@gmail.com>
2026-08-14 01:22:00 -0400

    Redirect legacy Vercel URLs to primary domain

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'cynador.vercel.app' }],
+        destination: 'https://cynador.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
   poweredByHeader: false,
 };
 module.exports = nextConfig;
